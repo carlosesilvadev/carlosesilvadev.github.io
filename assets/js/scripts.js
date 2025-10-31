6975f7830f1b4029789bd2bbe763eb4a9a4551bd
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2025-10-30 22:33:24 -0300

    feat: Exibir lista do menu quando clicar no botão
    Foi adicionado a lista do menu adaptando para cada tamanho da tela

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,11 +6,24 @@ function menuClicado(menuHamburguer){
 	if(menuHamburguer.querySelector('span').className != 'exibirSeta'){
 		for(; contador < tamanhoHamburguer; contador++){
 			menuHamburguer.querySelectorAll('span')[contador].classList.add("exibirSeta");
+			
+			document.querySelector('header').classList.add("exibirMenu");
 		}
+
+			document.querySelector('header>nav').classList.add("exibirMenu");
+
+			document.querySelectorAll('header>nav>ul>li').classList.add("exibirMenu");
+
 	} else {
 		for(; contador < tamanhoHamburguer; contador++){
 			menuHamburguer.querySelectorAll('span')[contador].classList.remove("exibirSeta");
 		}
+
+			document.querySelector('header').classList.remove("exibirMenu");
+
+			document.querySelector('header>nav').classList.remove("exibirMenu");
+
+			document.querySelectorAll('header>nav>ul>li').classList.remove("exibirMenu");
 	}
 	
 }
