186752927830b69836734e4383891bbc8523b457
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2025-10-31 10:50:53 -0300

    feat: Desenvolver efeito de texto sendo digitado
    Foi ajustado erros de busca de seletores no DOM ao clicar no menu e adicionado o favicon para eliminar todos os erros que apresentavam no console ao recarregar a página

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,19 +2,21 @@ function menuClicado(menuHamburguer){
 
 	let contador = 0; tamanhoHamburguer = menuHamburguer.querySelectorAll('header>nav>a>span').length;
 
-	
+	/*Verifica se não existe a class exibirSeta no primeiro span e em caso positivo é realizado a varredura em cada span e adicionado a classe exibirMenu e atribui essa mesma classe para os elementos nav e todos os li da lista do menu*/
 	if(menuHamburguer.querySelector('span').className != 'exibirSeta'){
 		for(; contador < tamanhoHamburguer; contador++){
 			menuHamburguer.querySelectorAll('span')[contador].classList.add("exibirSeta");
+		}
 			
 			document.querySelector('header').classList.add("exibirMenu");
-		}
 
 			document.querySelector('header>nav').classList.add("exibirMenu");
 
-			document.querySelectorAll('header>nav>ul>li').classList.add("exibirMenu");
+			document.querySelector('header>nav>ul>li').classList.add("exibirMenu");
 
 	} else {
+
+	/*Caso contrário realiza o mesmo processo acima, mas ao invés de adicionar a classe realiza a remoção da classe exibirSeta.*/
 		for(; contador < tamanhoHamburguer; contador++){
 			menuHamburguer.querySelectorAll('span')[contador].classList.remove("exibirSeta");
 		}
@@ -23,7 +25,27 @@ function menuClicado(menuHamburguer){
 
 			document.querySelector('header>nav').classList.remove("exibirMenu");
 
-			document.querySelectorAll('header>nav>ul>li').classList.remove("exibirMenu");
+			document.querySelector('header>nav>ul>li').classList.remove("exibirMenu");
 	}
 	
+}
+
+let paixoes = "desenvolvimento de sites", contadorLetra = 0;
+
+window.onload = function digitandoTexto(){
+			document.querySelector('span#apaixonado').innerHTML += paixoes.charAt(contadorLetra);
+			contadorLetra++;
+
+			setTimeout(digitandoTexto,50);
+
+			/*if(contadorLetra >= document.querySelector('span#apaixonado').innerHTML.length){
+				removendoTexto();
+			}
+
+			function removendoTexto(){
+				document.querySelector('#apaixonado').innerHTML = paixoes.slice(0,contadorLetra);
+				contadorLetra--;
+
+				setTimeout(removendoTexto,50);
+			}*/
 }
